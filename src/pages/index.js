79fd93c550f9a417101669cbd18b8eb02de768b9
Alexander Chapchuk <BigTows@yandex.ru>
2022-04-08 13:34:11 +0300

    Update .

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import {Header} from "../components/composed/header/Header.tsx";
+
+export default function Home() {
+    return (
+        <div>
+            <h1 className={"overflow-auto top-1 bg-black"}>Hello</h1>
+            <Header />
+        </div>
+    )
+}
