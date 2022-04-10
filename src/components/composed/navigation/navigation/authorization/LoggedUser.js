96299e38a4168168e9d564edb11dad4278372be4
Alexander Chapchuk <BigTows@yandex.ru>
2022-04-10 13:13:10 +0300

    PR fixes

--- src/components/composed/navigation/navigation/authorization/LoggedUser.js
+++ src/components/composed/navigation/navigation/authorization/LoggedUser.js
@@ -1,25 +1,25 @@
 import Image from "next/image";
-import {useEffect, useState} from "react";
-import {injector} from "../../../../../config/DependencyInjection";
+import {BASE_URL, injector} from "../../../../../config/DependencyInjection";
 import ProfileService from "../../../../../services/profile/ProfileService";
 import {connect} from "react-redux";
 
-export  function LoggedUser({profile}) {
+export function LoggedUser({profile}) {
 
     injector.get(ProfileService).loadProfileData()
-
-    return(
-        <div className={'flex gap-2 items-center opacity-70 hover:opacity-100 transition ease-in-out duration-500 cursor-pointer'}>
+    const baseUrl = injector.get(BASE_URL)
+    return (
+        <div
+            className={'flex gap-2 items-center opacity-70 hover:opacity-100 transition ease-in-out duration-500 cursor-pointer'}>
             {
                 !profile
-                ? null
-                : <>
-                    <div className={'rounded-xl overflow-hidden item-center'}>
-                        <Image src={`https://api-staging.devbuff.com/photo/profile/${profile.id}`} height={30} width={30} />
-                    </div>
-                    <span className={'text-xs text-black opacity-60'}>{profile.firstName || 'Bi-bu-bip'}</span>
-                    <span className={'text-xs text-gray-300'}>@{profile.userName}</span>
-                </>
+                    ? null
+                    : <>
+                        <div className={'rounded-xl overflow-hidden item-center'}>
+                            <Image src={`${baseUrl}/photo/profile/${profile.id}`} height={30} width={30}/>
+                        </div>
+                        <span className={'text-xs text-black opacity-60'}>{profile.firstName || 'Bi-bu-bip'}</span>
+                        <span className={'text-xs text-gray-300'}>@{profile.userName}</span>
+                    </>
             }
         </div>
     )
