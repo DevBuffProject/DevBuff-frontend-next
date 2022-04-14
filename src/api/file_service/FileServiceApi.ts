56cc3ad5227f4106189910e5b81d3fb0f39c376c
Roseskk <oleg419583@yandex.ru>
2022-04-14 14:04:29 +0300

    Image fixed

---
+++ src/api/file_service/FileServiceApi.ts
@@ -0,0 +1,34 @@
+import HttpClient from "../http/HttpClient";
+
+
+export default class FileServiceApi {
+
+    private static readonly BASE_PATH: string = "/file"
+    private readonly httpClient : HttpClient
+
+    constructor(httpClient : HttpClient) {
+
+        this.httpClient = httpClient
+    }
+
+    async getUserAvatar (uuid : string ) : Promise<string> {
+        return  await this.httpClient.get(`${FileServiceApi.BASE_PATH}/avatar/${uuid}`)
+    }
+
+    async getIdeaLogo (ideaId : string) : Promise<string> {
+        return await this.httpClient.get(`${FileServiceApi.BASE_PATH}/idea/${ideaId}/logo`)
+    }
+
+    async getUserFileName (userId : string, fileName : string) : Promise<string> {
+        return await this.httpClient.get(`${FileServiceApi.BASE_PATH}/image/${userId}/${fileName}`)
+    }
+
+    async uploadAvatar (image : string) : Promise<void> {
+        return await this.httpClient.post(`${FileServiceApi.BASE_PATH}/image`,image)
+    }
+
+    async updateAvatar (image : string) : Promise<string> {
+        return await this.httpClient.put(`${FileServiceApi.BASE_PATH}/avatar`,image)
+    }
+
+}
