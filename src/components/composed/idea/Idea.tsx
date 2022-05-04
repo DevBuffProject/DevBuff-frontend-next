4ae09a4a182c421d9227d7e3b687cb3bc042cac6
Roseskk <oleg419583@yandex.ru>
2022-05-04 11:50:56 +0300

    IdeaPage

---
+++ src/components/composed/idea/Idea.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function IdeaView () {
+    return(
+        <div>
+            hear
+        </div>
+    )
+}
--- src/pages/idea/[id].tsx
+++ src/pages/idea/[id].tsx
@@ -2,14 +2,13 @@ import {GetServerSidePropsContext} from "next/types";
 import {injector} from "../../config/DependencyInjection";
 import IdeaService from "../../services/idea/IdeaService";
 import Idea from "../../api/idea/objects/Idea";
+import IdeaView from "src/components/composed/idea/Idea";
 
 
 export default function IdeaPage(idea : Idea) {
     console.log(idea)
     return(
-        <div>
-            HEARING CAT
-        </div>
+        <IdeaView />
     )
 }
 
