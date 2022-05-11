c0e3349d780e1897ba1c0d2b73e70613aca48e72
Roseskk <oleg419583@yandex.ru>
2022-05-11 12:54:00 +0300

    IdeaExplore

--- src/pages/idea/explore/[id].tsx
+++ src/pages/idea/explore/[id].tsx
@@ -5,12 +5,18 @@ import IdeaService from "../../../services/idea/IdeaService";
 import Idea from "../../../api/idea/objects/Idea";
 import IdeaPage from "../[id]";
 
+// @ts-ignore
+import renderHTML from 'react-render-html';
+
 
 export default function ExploreTab(idea: Idea) {
     console.log(idea)
     return(
         <IdeaPage>
-            asdasdasd
+            <div className={'flex flex-col items-center justify-start bg-white md:p-2 p-1 rounded'}>
+                <span className={'text-center md:text-xl text-base font-montserratRegular'}>Описание</span>
+                <p className={'font-montserratLight md:text-base text-xs'}>{renderHTML(idea.text)}</p>
+            </div>
         </IdeaPage>
     )
 }
